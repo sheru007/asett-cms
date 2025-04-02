09f26a3dd0326d4b5ad0e07839d23a33e40d78fe
Ankaj Goyal <ankaj.goyal@apna.co>
2025-04-02 22:58:38 +0530

    uswds-setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+// import "@trussworks/react-uswds/lib/uswds.css";
+// import '@trussworks/react-uswds/lib/index.css';
+import {
+	GovBanner,
+  } from "@trussworks/react-uswds";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div>
+			<GovBanner/>
 			<h1>sheru</h1>
 		</div>
 	);
